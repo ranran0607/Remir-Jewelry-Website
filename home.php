fbebdb87e73a34479da7cd4c6f8db23e0ebcbe5b
Raniel Ronidel <rannielsronidel77@gmail.com>
2024-06-18 19:27:36 +0800

    

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="nav">
+            <img src="" alt="" class="logo log0-image">
+            <p class="logo logo-name">Remir's Jewelry</p>
+
+            <ul class="navbar-list">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">REVIEW</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-cart-shopping"></i>
+            
+            <!-- <button><a href="#">LOG IN</a></button>
+            <button><a href="#">SIGN UP</a></button> -->
+
+        </div>
+        <div class="content frst-content">
+            <h1>Discover the beauty of hand made jewelries</h1>
+            <p>Elevate your style with our stunning array of jewelry, where every <br> piece tells a story of time less elegance and sophistication.</p>
+        </div>
+    </div>
+
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="nav">
+            <div class="navbar-leftlist">
+                <img src="logo/diamond.png" alt="diamond" class="logo log0-image" height="40px" width="auto">
+                <a href="#" class="logo logo-name"><span class="owner-name">Remir</span> <br> Jewelry</a>
+            </div>
+            
+            <ul class="navbar-centerlist">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">REVIEW</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+
+            <div class="navbar-rightlist">
+                <button class="btn1 btn-login"><a href="#">LOG IN</a></button>
+                <button class="btn1 btn-signup"><a href="#">SIGN UP</a></button>
+            </div>
+            
+
+        </div>
+        <div class="model frst-content">
+            <div class="tag-line">
+                <h1>Discover the beauty <br> of hand made jewelries</h1>
+                <p>Elevate your style with our stunning array of jewelry, where every <br> piece tells a story of time less elegance and sophistication.</p>
+            </div>
+            <button class="btn btn-explorenow"><a href="#">Explore now! <i class="fa-solid fa-arrow-right"></i></a></button>
+        </div>
+        <div class="services">
+            <div class="service free-shipping">
+                <img src="icon/free-shipping.png" alt="Free shipping" class="icon icon-free-shipping">
+                <div class="descriptions">
+                    <h4 class="description">Free Shipping</h4>
+                    <p class="sub-description">Free shipping for order above $150</p>
+                </div>
+            </div>
+            <div class="service money-guarantee">
+                <img src="icon/money-guarantee.png" alt="Money Guarantee" class="icon icon-money-guarantee">
+                <div class="descriptions">
+                    <h4 class="description">Money Guarantee</h4>
+                    <p class="sub-description">Within 30 days for an exchange</p>
+                </div>
+            </div>
+            <div class="service online-support">
+                <img src="icon/online-support.png" alt="Online Support" class="icon icon-online-support">
+                <div class="descriptions">
+                    <h4 class="description">Online Support</h4>
+                    <p class="sub-description">24 hours a day, 7 days a week</p>
+                </div>
+            </div>
+            <div class="service flexible-payment">
+                <img src="icon/flexible-payment.png" alt="Flexible Payment" class="icon icon-flexible-payment">
+                <div class="descriptions">
+                    <h4 class="description">Flexible Payment</h4>
+                    <p class="sub-description">Pay with multiple credit cards</p>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="main-contents">
+        <h2 class="main-cnt-title">
+            Our Jewelry Collection
+        </h2>
+        <div class="main-content">
+            <div class="content first-content">
+                <img src="img/earrings.jpg" alt="Earrings" class="img-earrings" height="700px" width="auto">
+                <div class="prod-name">
+                    <p>Embossed Earrings</p>
+                    <p>$26.20</p>
+                </div>
+            </div>
+            <div class="content second-content">
+                <img src="img/ring.jpg" alt="ring" class="img-ring" height="700px" width="auto">
+                <div class="prod-name">
+                    <p>Faceted Cystal Ring</p>
+                    <p>$26.20</p>
+                </div>  
+            </div>
+            <div class="content third-content">
+                <img src="img/necklace.jpg" alt="necklace" class="img-necklace" height="700px" width="auto">
+                <div class="prod-name">
+                    <p>Beaded Double</p>
+                    <p>$26.20</p>
+                </div>
+            </div>
+        </div>
+        <div class="btn btn-view">
+            <button class="btn-view-all"><a href="#">View All</a></button>
+        </div>
+    </div>
+    <div class="trend-contents">
+        <h2 class="trend-designs-title">Trending Designs</h2>
+        <div class="trend-content">
+        
+            <div class="trend first-trnd">
+                <img src="img/trend-design1.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Luna Droplets</p>
+                    <p>$599 <span class="trctt-blur">$1000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design2.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Traditionl Wedding Bangles</p>
+                    <p>$999 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design3.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Radiant Circles</p>
+                    <p>$2990 <span class="trctt-blur">$3000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design4.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Diamond Ring</p>
+                    <p>$550 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design5.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Radiant Reflection</p>
+                    <p>$1000 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design6.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Enchanted Elegance</p>
+                    <p>$999 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design7.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Opal Embrace</p>
+                    <p>$999 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+
+            <div class="trend first-trnd">
+                <img src="img/trend-design8.jpg" alt="" class="" height="380px" width="380px">
+                <div class="trend-desciptions">
+                    <p>Whispering Heart</p>
+                    <p>$1599 <span class="trctt-blur">$2000</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="discounts-content">
+        <div class="disc-descriptions">          
+            <div class="disc-descrip-content">
+                <div class="disc-title">
+                    <h2>Get your Sindy Jewelry set today - 20% discounts-content applied</h>
+                </div>
+                <div class="disc-descript">
+                    <p>Discover the allure of exquisite craftsmanship at unbeatable prices! Shop now at our online jewelry store and enjoy a dazzling 20% discount on all items. Don't miss this limited-time offer to elevate your style with timeless elegance at a fraction of the cost.</p>
+                </div>
+                <div class="btn-shopnow">
+                    <button><a href="#">Shop now <i class="fa-solid fa-arrow-right"></i></a></button>
+                </div>
+            </div>
+            <div class="img-prod-to-be-promoted">
+                <img src="img/emerald-diamond-ring.png" alt="emerald diamond ring" class="img-promoted-discount" height="400px" width="400px">
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
+
